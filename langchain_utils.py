22430b810327a629794991785fc3ca4f0ae1b176
Natnael Endale <n87294578@gmail.com>
2026-06-16 17:53:56 +0300

    add the retriver

---
+++ langchain_utils.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from typing import List
+from langchain_core.documents import Document
+import os
+from chroma_utils import vectorstore
+
+retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+
+output_parser = StrOutputParser()
