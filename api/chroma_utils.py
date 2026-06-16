9361c777645b2a2472c969abac40fdae4ec050a4
Natnael Endale <n87294578@gmail.com>
2026-06-16 23:23:21 +0300

    add chroma

--- api/chroma_utils.py
+++ api/chroma_utils.py
@@ -1,25 +1,52 @@
-from pydantic import BaseModel, Field
-from enum import Enum
-from datetime import datetime
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredHTMLLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from typing import List
+from langchain_core.documents import Document
+import os
 
-class ModelName(str, Enum):
-    GPT4_O = "gpt-4o"
-    GPT4_O_MINI = "gpt-4o-mini"
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
+embedding_function = OpenAIEmbeddings()
+vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
 
-class QueryInput(BaseModel):
-    question: str
-    session_id: str = Field(default=None , description="Unique identifier for the session. If not provided, a new one will be generated.")
-    model: ModelName = Field(default=ModelName.GPT4_O_MINI)
+def load_and_split_document(file_path: str) -> List[Document]:
+    if file_path.endswith('.pdf'):
+        loader = PyPDFLoader(file_path)
+    elif file_path.endswith('.docx'):
+        loader = Docx2txtLoader(file_path)
+    elif file_path.endswith('.html'):
+        loader = UnstructuredHTMLLoader(file_path)
+    else:
+        raise ValueError(f"Unsupported file type: {file_path}")
+    
+    documents = loader.load()
+    return text_splitter.split_documents(documents)
 
-class QueryResponse(BaseModel):
-    answer: str
-    session_id: str
-    model: ModelName
+def index_document_to_chroma(file_path: str, file_id: int) -> bool:
+    try:
+        splits = load_and_split_document(file_path)
+        
+        # Add metadata to each split
+        for split in splits:
+            split.metadata['file_id'] = file_id
+        
+        vectorstore.add_documents(splits)
+        # vectorstore.persist()
+        return True
+    except Exception as e:
+        print(f"Error indexing document: {e}")
+        return False
 
-class DocumentInfo(BaseModel):
-    id: int
-    filename: str
-    upload_timestamp: datetime
-
-class DeleteFileRequest(BaseModel):
-    file_id: int
+def delete_doc_from_chroma(file_id: int):
+    try:
+        docs = vectorstore.get(where={"file_id": file_id})
+        print(f"Found {len(docs['ids'])} document chunks for file_id {file_id}")
+        
+        vectorstore._collection.delete(where={"file_id": file_id})
+        print(f"Deleted all documents with file_id {file_id}")
+        
+        return True
+    except Exception as e:
+        print(f"Error deleting document with file_id {file_id} from Chroma: {str(e)}")
+        return False
